92a76071f7d58d20f5924dddf750c5ed4d08a078
Mark Günzel <mark.guenzel@gmx.de>
2026-03-10 16:02:25 +0100

    Added MDX

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AppNavbar from './components/AppNavbar'
 import AppSidebar from './components/AppSidebar'
 import { SidebarProvider } from './components/ui/sidebar'
 import { ThemeProvider } from './components/ui/theme-provider'
+import Introduction from './chapters/0-introduction.mdx'
 
 function App() {
   return (
@@ -12,8 +13,12 @@ function App() {
           <AppSidebar />
           <main className='w-full'>
             <AppNavbar />
-            <h1>Introduction</h1>
-            <h2>Perlin Noise</h2>
+
+            <div className='flex justify-center p-8'>
+              <article className="prose prose-slate lg:prose-xl dark:prose-invert text-left">
+                <Introduction />
+              </article>
+            </div>
           </main>
         </SidebarProvider>
       </div>
---
+++ src/chapters/mdx.d.ts
@@ -0,0 +1,6 @@
+declare module '*.mdx' {
+  import type { ElementType } from 'react'
+
+  const MDXContent: ElementType
+  export default MDXContent
+}
